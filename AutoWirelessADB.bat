33e9c51f95bcac78eb42f6b3cdbea8fdcf400704
Nirbhay Sirsikar <108933885+NirbhaySirsikar@users.noreply.github.com>
2023-09-11 22:21:47 +0530

    Initial commit

---
+++ AutoWirelessADB.bat
@@ -0,0 +1,27 @@
+@echo off
+setlocal enabledelayedexpansion
+
+adb tcpip 5555
+pause
+
+adb devices
+
+for /f "tokens=1,* delims=:" %%a in ('adb shell ip -f inet addr show wlan0 ^| findstr /n "^"') do (
+    if "%%a"=="2" (
+        set "IP_ADDRESS=%%b"
+    )
+	
+)
+
+echo command fetched: !IP_ADDRESS!
+
+for /f "tokens=2 delims=/ " %%a in ("!IP_ADDRESS!") do (
+    set "EXTRACTED_IP=%%a"
+)
+
+echo Extracted IP Address: !EXTRACTED_IP!
+
+adb connect !EXTRACTED_IP!:5555
+
+pause
+endlocal
