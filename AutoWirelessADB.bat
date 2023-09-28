f4fa3e4f9a394fd02324214bdc8d48cdf6d91140
NirbhaySirsikar <nirbhaysirsikar123@gmail.com>
2023-09-28 12:34:22 +0530

    Fixed bug due to variable name confusion

--- AutoWirelessADB.bat
+++ AutoWirelessADB.bat
@@ -8,23 +8,23 @@ adb devices
 
 for /f "tokens=1,* delims=:" %%a in ('adb shell ip -f inet addr show wlan0 ^| findstr /n "^"') do (
     if "%%a"=="2" (
-        set "IP_ADDRESS=%%b"
+        set "EXTRACTED_IP=%%b"
     )
 	
 )
 
-echo command fetched: !IP_ADDRESS!
+echo command fetched: !EXTRACTED_IP!
 
-for /f "tokens=2 delims=/ " %%a in ("!IP_ADDRESS!") do (
-    set "EXTRACTED_IP=%%a"
+for /f "tokens=2 delims=/ " %%a in ("!EXTRACTED_IP!") do (
+    set "IP_ADDRESS=%%a"
 )
 
-echo Extracted IP Address: !EXTRACTED_IP!
+echo Extracted IP Address: !IP_ADDRESS!
 
-adb connect !EXTRACTED_IP!:5555
+adb connect !IP_ADDRESS!:5555
 
 echo.
-echo You can now remove your device
+echo You can now disconnect your device
 
 pause
 endlocal
